bbb2841cec7794e25edfe38f3dc251ce88cb0c8e
Andrew Petersen <andrew.petersen15@gmail.com>
2016-10-03 18:59:01 -0500

    fixed Azure port and IP stuff and a broken module ref

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var bodyParser = require("body-parser");
 var express = require("express");
 var alexaSkill = require("./alexa");
+var nutritionApi = require("./services/nutritionApi");
+var config = { port: process.env.PORT || 3000 };
 
-var config = { port: 3000 };
 var app = express();
 
 // we need the body parser so we can get at the POST body that Amazon sends
@@ -21,7 +22,7 @@ app.get("/nutrition/:search", function(req, res){
 
 
 // Start the web server on the specified port
-app.listen(config.port, function() {
+app.listen(config.port, process.env.IP, function() {
     console.log("Server running on port " + config.port)
 });
 
